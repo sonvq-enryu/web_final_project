d4ce377bedc0ec94846de97b5584ae47c790d5a1
tuhuyvan123 <tuhuyvan123@gmail.com>
2021-05-10 19:03:34 +0700

    dev form

--- main.js
+++ main.js
@@ -340,3 +340,28 @@ function Filevalidation() {
 /* **********************************************************
                         END OF LIMIT FILE UPLOAD SIZE
 *********************************************************/
+/* **********************************************************
+                        LIMIT FILE UPLOAD SIZE
+*********************************************************/
+function Filevalidation() {
+    const fi = document.getElementById('apk');
+    // Check if any file is selected.
+    if (fi.files.length > 0) {
+        for (const i = 0; i <= fi.files.length - 1; i++) {
+
+            const fsize = fi.files.item(i).size;
+            const file = Math.round((fsize / 1024));
+            // The size of the file.
+            if (file >= 104857600) {
+                alert(
+                    "File too Big, please select a file less than 4mb");
+            } else {
+                document.getElementById('size').innerHTML = '<b>' +
+                    file + '</b> KB';
+            }
+        }
+    }
+}
+/* **********************************************************
+                        END OF LIMIT FILE UPLOAD SIZE
+*********************************************************/
