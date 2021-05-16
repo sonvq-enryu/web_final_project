10739318efd3c40595fb191c54ac8a41d16d9928
unknown <exp.qson@gmail.com>
2021-05-16 21:21:59 +0700

    update

--- db.php
+++ db.php
@@ -1015,8 +1015,8 @@
             $mail->CharSet = 'UTF-8';                          //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'exp.critical@gmail.com';                     //SMTP username
-            $mail->Password   = 'mypasswordis192';                               //SMTP password
+            $mail->Username   = 'castlerealestatead@gmail.com';                     //SMTP username
+            $mail->Password   = 'realestate123';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
